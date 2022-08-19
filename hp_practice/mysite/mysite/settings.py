f9aa4b49aa81c76418b6ab3c06aeaca7c2871c79
44Chick <49892292+44Chick@users.noreply.github.com>
2022-08-19 20:00:31 +0900

    V0.3

--- hp_practice/mysite/mysite/settings.py
+++ hp_practice/mysite/mysite/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login_app',
+    'myapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/login/'
+
